d1c83f6e30bcce6f9700a0c36a25d9c615a11866
Eduardo Beyra <eduardobeyra@hotmail.com>
2022-01-17 12:28:17 -0500

    Practice

--- worksheet.js
+++ worksheet.js
@@ -31,22 +31,45 @@ const characters = [
 
 //***MAP***
 //1. Get array of all names
-const charNames = characters.map(function () {
-    names = [];
-    for (let name in characters) {
-        names.push(name.name);
-        return names;
-    }
+const charNames = characters.map(function (charNames) {
+    return charNames.name
 })
 //2. Get array of all heights
+const charHeights = characters.map(function (charHeight) {
+    return charHeight.height;
+  })
 //3. Get array of objects with just name and height properties
+const charNameHeight = characters.map(function(chars) {
+    return {
+      name: chars.name,
+      height: chars.height
+    }
+  })
 //4. Get array of all first names
-
+const firstNames = characters.map(function (fname) {
+    return fname.name.split(" ").splice(0, 1)
+  })
 //***REDUCE***
 //1. Get total mass of all characters
+const totalMass = characters.reduce(function (sum, char) {
+    sum += char.mass
+    return sum;
+  }, 0)
 //2. Get total height of all characters
+const totalHeight = characters.reduce(function (sum, char) {
+    sum += char.height
+    return sum;
+  }, 0)
 //3. Get total number of characters by eye color
+const colorChar = characters.reduce(function (sum, char) {
+    sum += char.eye_color.length
+    return sum;
+  }, 0)
 //4. Get total number of characters in all the character names
+const nameChar = characters.reduce(function (sum, char) {
+    sum += char.name.length
+    return sum;
+  }, 0)
 
 //***FILTER***
 //1. Get characters with mass greater than 100
